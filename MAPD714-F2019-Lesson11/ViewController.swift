1d4173c9b1640749ab59159dfa60e2f61053bec6
pratiksha kathiriya <pratikshakakathiriya22597@gmail.com>
2019-11-20 22:32:31 -0500

    initial commit

--- MAPD714-F2019-Lesson11/ViewController.swift
+++ MAPD714-F2019-Lesson11/ViewController.swift
@@ -7,14 +7,94 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    //Login
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var showMessage: UILabel!
+    
+    //signUp
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var statusText: UILabel!
+    
+    @IBAction func signupButton(_ sender: Any)
+    {
+        guard let email = self.emailText.text, let password=self.passwordText.text
+        else
+        {
+            self.statusText.text = "email/password can't be empty"
+        return
+        }
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        // [START_EXCLUDE]
+          guard let user = authResult?.user, error == nil else
+          {
+            self.statusText.text = error!.localizedDescription
+            return
+          }
+            self.statusText.text = "\(user.email!) created"
+        
+        }
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton)
+    {
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { [weak self] authResult, error in
+            
+            //print(self!.email.text)
+            //print(self!.password.text)
+          //guard let strongSelf = self else { return }
+          // [START_EXCLUDE]
+         
+            if let error = error
+            {
+                self!.showMessage.text = error.localizedDescription;              return
+            }
+            else
+            {
+                self!.showMessage.text = "Successful Login";
+            }
+        }
     }
 
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
 
+    
+    
+    
+    func setTitleDisplay(_ user: User?) {
+      if let name = user?.displayName {
+        self.navigationItem.title = "Welcome \(name)"
+      } else {
+        self.navigationItem.title = "Authentication Example"
+      }
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+      super.viewWillAppear(animated)
+      // [START auth_listener]
+      handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+      }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      // [START remove_auth_listener]
+      Auth.auth().removeStateDidChangeListener(handle!)
+      // [END remove_auth_listener]
+    }
+    
+    
 }
 
